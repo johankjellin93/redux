dab273f36be9495befe0735eb224ed2554382cd2
Unknown <johankjellin93@gmail.com>
2018-05-06 22:52:05 +0200

    ch 3

--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -5,6 +5,8 @@ import { render } from 'react-dom';
 import css from './styles/style.styl';
 
 import Main from './components/Main';
+import Single from './components/Single';
+import PhotoGrid from './components/PhotoGrid';
 
 render(<Main/>, document.getElementById('root'));
 
